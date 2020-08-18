ebe392c8daf23726c9606ad577bbc85fe1fe7fde
ThanakritTG8 <tgrock1997@gmail.com>
2020-08-18 23:03:26 +0700

    commit to git

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <apexchart width="500" type="line" :options="options" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Hello-World',
+    data:() => ({
+        options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    })
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/layout/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="nav-bar">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#"> <h4><b>Detect Word Sense</b></h4></b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+            <b-button v-b-toggle.sidebar-backdrop>   <b-icon icon="justify" font-scale="1.5"></b-icon></b-button>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+      <b-sidebar
+      id="sidebar-backdrop"
+      title="Sidebar with backdrop"
+      :backdrop-variant="variant"
+      backdrop
+      shadow
+    >
+      <div class="px-3 py-2">
+        <b-form-group label="Backdrop variant" label-for="backdrop-variant">
+          <b-form-select id="backdrop-variant" v-model="variant" :options="variants"></b-form-select>
+        </b-form-group>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+<script>
+export default {
+  name: "nav",
+};
+</script>
