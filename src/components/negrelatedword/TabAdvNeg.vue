8b60ad9aeb06973ef16a73aa49b4bfbf96d96c5a
ThanakritTG8 <tgrock1997@gmail.com>
2020-10-01 23:47:35 +0700

    update api on related page

--- src/components/negrelatedword/TabAdvNeg.vue
+++ src/components/negrelatedword/TabAdvNeg.vue
@@ -40,7 +40,7 @@ export default {
   }),
   mounted() {
     this.$axios
-      .get("http://localhost:5000/senten/text/test/NOUN")
+      .get("http://api.playz-th.com:5000/postgards/NEGADV")
       .then(({ data }) => {
         for (let key in data) {
           this.item = data;
