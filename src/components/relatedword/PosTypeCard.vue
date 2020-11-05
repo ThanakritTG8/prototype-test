716af39fddd08017cfc03f74eeac55603f68f2e1
Karin <not.karin@hotmail.com>
2020-11-05 15:17:02 +0700

    Overlay Related page

--- src/components/relatedword/PosTypeCard.vue
+++ src/components/relatedword/PosTypeCard.vue
@@ -1,43 +1,51 @@
 <template>
   <div id="pos-type-card">
-    <b-tabs
-      fill
-      class="b-card shadow p-3 mb-5 bg-white rounded"
-      align="center"
-      active-nav-item-class="font-weight-bold text-uppercase text-success"
-    >
-      <b-tab title="Place" class="tab" active>
-        <tab-noun />
-      </b-tab>
-      <b-tab title="Action" class="tab"> 
-        <tab-verb />
-      </b-tab>
-      <b-tab title="Emotion" class="tab"> 
-        <tab-adj />
-      </b-tab>
-      <b-tab title="Character" class="tab"> 
-        <tab-adv />
-      </b-tab>
-    </b-tabs>
+    <b-overlay :show="busyLine" rounded="lg" opacity="0.6">
+      <b-tabs
+        fill
+        class="b-card shadow p-3 mb-5 bg-white rounded"
+        align="center"
+        active-nav-item-class="font-weight-bold text-uppercase text-success"
+      >
+        <b-tab title="Place" class="tab" active>
+          <tab-noun />
+        </b-tab>
+        <b-tab title="Action" class="tab">
+          <tab-verb />
+        </b-tab>
+        <b-tab title="Emotion" class="tab">
+          <tab-adj />
+        </b-tab>
+        <b-tab title="Character" class="tab">
+          <tab-adv />
+        </b-tab>
+      </b-tabs>
+    </b-overlay>
   </div>
 </template>
 
 <script>
-import TabNoun from '@/components/relatedword/TabNoun'
-import TabVerb from '@/components/relatedword/TabVerb'
-import TabAdj from '@/components/relatedword/TabAdj'
-import TabAdv from '@/components/relatedword/TabAdv'
+import TabNoun from "@/components/relatedword/TabNoun";
+import TabVerb from "@/components/relatedword/TabVerb";
+import TabAdj from "@/components/relatedword/TabAdj";
+import TabAdv from "@/components/relatedword/TabAdv";
 export default {
   name: "pos-type-card",
   components: {
     TabNoun,
     TabVerb,
     TabAdj,
-    TabAdv
+    TabAdv,
   },
-  data: () => ({}),
-  mounted() {},
-  methods: {}
+  data: () => ({
+    busyLine: true,
+  }),
+  mounted() {
+    setTimeout(() => {
+      this.busyLine = false;
+    }, 10000);
+  },
+  methods: {},
 };
 </script>
 
